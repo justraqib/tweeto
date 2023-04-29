eed8d726ab5c938f81c8bbd7d74dc7929c4e84e9
Raqib Ahmed <justraqib@gmail.com>
2023-04-29 16:28:12 +0530

    add tailwind to the project

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../utils/auth';
+import { Inter } from '@next/font/google';
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   )
 }
